4f3ee6753d16ae3be0f64f7edcd62fe56be12dd9
YoonKeumJae <ykj1512@gmail.com>
2025-07-01 14:53:48 +0900

    fix: add all colors into style.css

--- src/scripts/components/footer-component.ts
+++ src/scripts/components/footer-component.ts
@@ -7,7 +7,7 @@ class FooterComponent extends HTMLElement {
     this.innerHTML = `
       <footer class="w-screen text-center py-4 bg-docker-primary text-white relative bottom-0">
         © 2025 Docker 한국어 문서 프로젝트 |
-        <a href="https://github.com/docker-ko/docker-ko.github.io" class="hover:underline text-[#b0c4de]">GitHub</a>
+        <a href="https://github.com/docker-ko/docker-ko.github.io" class="hover:underline text-blue-light">GitHub</a>
       </footer>
     `;
   }
