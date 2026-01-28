faefeb67e0a7053c7ce6400d70b793a8fbc6813c
krsy0411 <90031820+krsy0411@users.noreply.github.com>
2026-01-28 02:06:56 +0000

    Deploying to gh-pages from @ docker-ko/docker-ko.github.io@46a93016d4ff3128d3c7eb42e0ded0cbf758c041 🚀

--- src/scripts/components/index.ts
+++
@@ -1,7 +0,0 @@
-import './footer-component';
-import './header-component';
-import './card-component';
-import './button-component';
-import './nav-component';
-import './contributor-component';
-import './home-link-card-component';
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  darkMode: 'media', // 또는 'class'
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './public/**/*.md',
-    './src/scripts/components/*.{js,ts,jsx,tsx}'
-  ],
-};
